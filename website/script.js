eb76257772345bf2ede9218c9da9e993a25daaea
DaveTanKY <dave21sg@gmail.com>
2021-02-08 00:40:38 +0800

    added getting data from api

---
+++ website/script.js
@@ -0,0 +1,9 @@
+async function getData() {
+    const url = 'https://api.hearthstonejson.com/v1/latest/enUS/cards.json';
+    const response = await fetch(url);
+    const data = await response.json();
+    cardList = data;
+    console.log(data);
+}
+
+getData();
