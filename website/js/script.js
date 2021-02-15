456f9ab345514ac02f00ffe06052ca1a604915ac
Timothy Seow <s10204778@connect.np.edu.sg>
2021-02-16 00:54:53 +0800

    restricted retrieving of data to only 4 classes

--- website/js/script.js
+++ website/js/script.js
@@ -9,24 +9,26 @@ fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards", {
 })
 .then(response => response.json())
 .then(function (data){
-    let card = data.Basic;
-    console.log(card);
-    cardList = card;
+    let set = data;
+    var count = 0;
+    $.each(set, function (key, obj) {
+        if (count < 4){
+            $.each(obj, function (index, value){
+                cardList.push(value.cardId);
+            })
+            count += 1;
+        }
+    })
 })
+.then(() => getcardinfo())
 .catch(err => {
     console.error(err);
 });
 
-var testing = [];
-function  test()
-{
-    var count = 0;
-    console.log('test');
+function getcardinfo(){
     $.each(cardList, function (index, value){
-        count += 1;
-        
-        
-        fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${value.name}`, {
+        console.log(value)
+        fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${value}`, {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "6656a0b8afmsha230c04208cbd77p13668djsn8507de8fe1ec",
@@ -35,32 +37,22 @@ function  test()
         })
         .then(res => res.json())
         .then(function (info){
-            console.log('success');
-            info.forEach(data => {
-                if(data.collectible == true)
-                {
-                    testing.push(data);
-                }
-            });
-             
-            
-            
+            var cardinfo = info[info.length - 1]
+            if ("img" in cardinfo && cardinfo.collectible == true){
+                $('.card-container').append(
+                    $('<div/>')
+                    .append(
+                        $('<img/>')
+                        .addClass("indiv-card")
+                        .attr("src", cardinfo.img)
+                        
+                    )
+                )
+            }
         })
         .catch(err => {
             console.error(err);
-            
         });
         
     })
-    console.log(count);
-    
-}
-
-function test2()
-{
-    
-    testing.forEach(card => {
-        console.log(card);
-    });
-    
 }
