86b85b05339f253e9739969a485c75628e518dbb
DaveTanKY <dave21sg@gmail.com>
2021-02-16 21:40:24 +0800

    added lottie animation and answer checking to quiz

--- website/js/quiz.js
+++ website/js/quiz.js
@@ -1,9 +1,26 @@
 
 cardList = [];
 cards = [];
-function generate() {
-    alert('generate');
 
+var answer = 0;
+var score = 0;
+
+function startGame() {
+    
+    if (418 === cards.length) {
+        questionRandomizer();
+        $("#submitBtn").attr("onclick", "checkAnswer()");
+        $("#submitBtn").text("Submit");
+    }
+    else
+    {
+        alert("please wait for the quiz to load!")
+        console.log(cards.length);
+    }
+}
+
+generate();
+function generate() {
     fetch("https://omgvamp-hearthstone-v1.p.rapidapi.com/cards", {
         "method": "GET",
         "headers": {
@@ -16,8 +33,8 @@ function generate() {
             console.log(data);
             var count = 0;
             $.each(data, function (key, obj) {
-                if (count < 4){
-                    $.each(obj, function (index, value){
+                if (count < 4) {
+                    $.each(obj, function (index, value) {
                         cardList.push(value.cardId);
                     })
                     count += 1;
@@ -30,188 +47,188 @@ function generate() {
         });
 }
 
-function getcardinfo(){
-    $.each(cardList, function (index, value){
-        
+function getcardinfo() {
+    var cardCount = 0;
+
+    $(".quiz-loading").remove();
+    $("#quiz-container").append("<div class='chosen-card'>" +
+        "<div id='image-name'>Card Name</div>" +
+        "<div class='content-container'>" +
+        "<img id='quiz-image' src='images/card-back.png'>" +
+        "<div class='quiz-question'>" +
+        "<div id='question-title'>quiz question</div>" +
+        "<form class='question-answers'>" +
+        "<input class='answer' type='radio' id='answer1' name='quiz' value='1'>" +
+        "<label id='label1' for='answer1'>Answer 1</label><br>" +
+        "<input class='answer' type='radio' id='answer2' name='quiz' value='2'>" +
+        "<label id='label2' for='answer2'>Answer 2</label><br>" +
+        "<input class='answer' type='radio' id='answer3' name='quiz' value='3'>" +
+        "<label id='label3' for='answer3'>Answer 3</label><br>" +
+        "<input class='answer' type='radio' id='answer4' name='quiz' value='4'>" +
+        "<label id='label4' for='answer4'>Answer 4</label><br><br><br>" +
+        "<button type='button' onclick='startGame()' id='submitBtn'>Start</button>" +
+        "</form>" +
+        "</div>" +
+        "</div>" +
+        "</div>");
+
+    $.each(cardList, function (index, value) {
+
         fetch(`https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/${value}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "6656a0b8afmsha230c04208cbd77p13668djsn8507de8fe1ec",
-            "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "6656a0b8afmsha230c04208cbd77p13668djsn8507de8fe1ec",
+                "x-rapidapi-host": "omgvamp-hearthstone-v1.p.rapidapi.com"
             }
         })
-        .then(res => res.json())
-        .then(function (info){
-            var cardinfo = info[info.length - 1]
-            if ("img" in cardinfo && cardinfo.collectible == true){
-                cards.push(cardinfo);
-                console.log('success');
-            }
-        })
-        .catch(err => {
-            console.error(err);
-        });
+            .then(res => res.json())
+            .then(function (info) {
+                var cardinfo = info[info.length - 1]
+                if ("img" in cardinfo && cardinfo.collectible == true) {
+                    cards.push(cardinfo);
+                    console.log('success');
+                    cardCount += 1;
+                }
+            })
+            .catch(err => {
+                console.error(err);
+            });
+
+
     })
-
-
 }
 
-function test()
-{
-    
+function questionRandomizer() {
+
+
     var cardNumber = Math.floor(Math.random() * (cards.length));
     var card = cards[cardNumber];
     $("#image-name").text(card.name);
     $("#quiz-image").attr("src", card.img);
 
-    // what set
-    // what class
-    // type
-    // cost
-
-    var questionNumber = Math.floor(Math.random() * (4)) + 1;
+    var questionNumber = Math.floor(Math.random() * (3)) + 1;
     var newarray;
 
-    
-    if(questionNumber === 1)
-    {
+
+
+    if (questionNumber === 1) {
         $("#question-title").text("What class does this card belong to?")
         newarray = getClass(card.playerClass);
-        randomizer(newarray);
+        answer = randomizer(newarray, card.playerClass);
     }
-    else if(questionNumber === 2)
-    {
+    else if (questionNumber === 2) {
         $("#question-title").text("What is the rarity of this card?")
         newarray = getRarity(card.rarity);
-        randomizer(newarray);
+        answer = randomizer(newarray, card.rarity);
     }
-    else if(questionNumber === 3)
-    {
+    else if (questionNumber === 3) {
         $("#question-title").text("How much does it cost to play this card?")
         newarray = getCost(card.cost);
-        randomizer(newarray);
+        answer = randomizer(newarray, card.cost);
     }
-
 
 }
 
-function getCost(x)
-{
+function getCost(x) {
     var newArray = [x];
     var cardNumber;
     var newcard;
-    while(newArray.length != 4)
-    {
+    while (newArray.length != 4) {
         cardNumber = Math.floor(Math.random() * (cards.length));
         newcard = cards[cardNumber];
-        
 
-        if(newArray.includes(newcard.cost))
-        {
+
+        if (newArray.includes(newcard.cost)) {
             continue;
         }
-        else
-        {
+        else {
             newArray.push(newcard.cost);
         }
-        
+
     }
-    console.log(newArray)
+
     return newArray;
 }
 
-function getSet(x)
-{
+function getRarity(x) {
     var newArray = [x];
     var cardNumber;
     var newcard;
-    while(newArray.length != 4)
-    {
+    while (newArray.length != 4) {
         cardNumber = Math.floor(Math.random() * (cards.length));
         newcard = cards[cardNumber];
-        console.log('hi');
-        console.log(newcard);
-        console.log(newcard.cardSet);
 
-        if(newArray.includes(newcard.cardSet))
-        {
+        if (newArray.includes(newcard.rarity)) {
             continue;
         }
-        else
-        {
-            newArray.push(newcard.cardSet);
-        }
-        
-    }
-    console.log(newArray)
-    return newArray;
-}
-
-function getRarity(x)
-{
-    var newArray = [x];
-    var cardNumber;
-    var newcard;
-    while(newArray.length != 4)
-    {
-        cardNumber = Math.floor(Math.random() * (cards.length));
-        newcard = cards[cardNumber];
-        console.log('hi');
-        console.log(newcard);
-        console.log(newcard.rarity);
-
-        if(newArray.includes(newcard.rarity))
-        {
-            continue;
-        }
-        else
-        {
+        else {
             newArray.push(newcard.rarity);
         }
-        
+
     }
-    console.log(newArray)
+
     return newArray;
 }
 
-function getClass(x)
-{
+function getClass(x) {
     var newArray = [x];
     var cardNumber;
     var newcard;
-    while(newArray.length != 4)
-    {
+    while (newArray.length != 4) {
         cardNumber = Math.floor(Math.random() * (cards.length));
         newcard = cards[cardNumber];
-        console.log('hi');
-        console.log(newcard);
-        console.log(newcard.playerClass);
 
-        if(newArray.includes(newcard.playerClass))
-        {
+
+        if (newArray.includes(newcard.playerClass)) {
             continue;
         }
-        else
-        {
+        else {
             newArray.push(newcard.playerClass);
         }
-        
+
     }
-    console.log(newArray)
     return newArray;
 }
 
 
-function randomizer(list)
-{
-    for(let i = list.length - 1; i > 0; i --)
-    {
+function randomizer(list, answer) {
+    for (let i = list.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [list[i], list[j]] = [list[j], list[i]];
     }
-    
-    for(let x = 0; x < list.length; x++)
-    {
-        $(`#label${x+1}`).text(list[x]);
+
+    for (let x = 0; x < list.length; x++) {
+        $(`#label${x + 1}`).text(list[x]);
     }
+
+    var index = list.indexOf(answer) + 1
+    console.log(list);
+    console.log(index);
+    return index;
+}
+
+
+function checkAnswer() {
+    
+    var check = document.querySelector('input[name = "quiz"]:checked').value;
+    if (check != null) {
+        if (check == answer) {
+            alert(check)
+            alert('Congratulations! You got the correct answer!');
+        }
+        else {
+            alert("Unfortunately that was not the correct answer!");
+        }
+        nextQuestion();
+    }
+    else {
+        alert("Please select an option!");
+    }
+    
+}
+
+function nextQuestion()
+{
+        $("#submitBtn").attr("onclick", "startGame()");
+        $("#submitBtn").html("Next Question");
 }
